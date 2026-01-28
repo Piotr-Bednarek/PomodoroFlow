ca33bc4ac64dfde090ba644b94e882839319c704
Piotr Bednarek <72524343+Piotr-Bednarek@users.noreply.github.com>
2026-01-29 00:58:11 +0100

    batman i feat:wyskakuje okienko

---
+++ src/main/java/org/piotr/MainApp.java
@@ -0,0 +1,25 @@
+package org.piotr;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Pomodoro Flow label");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 400);
+
+        stage.setTitle("Pomodoro Flow");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
